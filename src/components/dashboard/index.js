4014f8742137500c4d3cf6adc9882bd9dd429015
krishna <keshri819@gmail.com>
2022-03-21 00:33:24 +0530

    Right Side Dashboard CSS added

--- src/components/dashboard/index.js
+++ src/components/dashboard/index.js
@@ -1,15 +1,19 @@
-import React from "react";
-
 import { capitalizeFirst, numberWithCommas } from "../../utils";
 import { dashboardCountKeys } from "../../constants/dashboard";
 
-export const Dashboard = (props) => {
-  const { data } = props;
+export const Dashboard = ({ type, data, setType }) => {
   return (
     <div className="dashboard-counts">
-      {dashboardCountKeys.map((dashboardCountKey) => {
+      {dashboardCountKeys.map((dashboardCountKey, index) => {
         return (
-          <div>
+          <div
+            className={`dashboard-count animate-${
+              index + 1
+            } dashboard-count-${dashboardCountKey} ${
+              type === dashboardCountKey ? "dashboard-count-active" : ""
+            }`}
+            onClick={() => setType(dashboardCountKey)}
+          >
             <div className="dashboard-count-label">
               {capitalizeFirst(dashboardCountKey)}
             </div>
