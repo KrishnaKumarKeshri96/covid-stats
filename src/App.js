4ea3191441175c18eb732035e1f0ef070d289653
krishna <keshri819@gmail.com>
2022-03-20 16:15:52 +0530

    Api Is Working Fine

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import { useEffect, useState } from "react";
+
 import { Loader } from "./components/loader/index";
+import { getBaseData } from "./api/getBaseData";
 
 function App() {
-  return <Loader />;
+  const [data, setData] = useState([]);
+  console.log(data);
+
+  useEffect(() => {
+    getBaseData().then((data) => {
+      setData(data);
+    });
+  }, []);
+  return data.length ? <h1>Got Data</h1> : <Loader />;
 }
 
 export default App;
---
+++ src/api/getBaseData.js
@@ -0,0 +1,12 @@
+import { BASE_API_ROUTE } from "../constants/apiConstants";
+
+export const getBaseData = async () => {
+  const response = await fetch(BASE_API_ROUTE);
+  if (!response.ok) throw response;
+  else {
+    const responseJSON = await response.json();
+    console.log(responseJSON);
+
+    return responseJSON;
+  }
+};
